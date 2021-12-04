006433090ebcca7d3dce3651585f9b862d5817f1
Wojciech Olech (SteelPh0enix) <steelphoenix2137@gmail.com>
2021-12-04 17:00:38 +0100

    added field reading code to one of the examples

--- Python/multiple_messages_buffer_example/read_messages.py
+++ Python/multiple_messages_buffer_example/read_messages.py
@@ -4,6 +4,25 @@ import struct
 INPUT_FILENAME = 'companies.bin'
 
 
+def person_to_string(person: Person) -> str:
+    return '[ID: {0}] {1} ({2})'.format(person.id, person.name, person.email)
+
+
+def company_to_string(company: Company) -> str:
+    employees_count = len(company.employees)
+    company_string = 'Name: {0}\nDirector: {1}\nEmployees count: {2}\nEmployees:\n'.format(
+        company.name, person_to_string(company.director), employees_count)
+
+    for employee in company.employees:
+        company_string += '\t{0}\n'.format(person_to_string(employee))
+
+    # also works like that:
+    # for i in range(employees_count):
+    # company_string += '\t{0}\n'.format(person_to_string(company.employees[i]))
+
+    return company_string
+
+
 def main():
     print('This example reads the messages stored in file and re-constructs them')
 
@@ -21,7 +40,7 @@ def main():
             company = Company()
             company.ParseFromString(message_data)
 
-            print("Read message:\n{0}".format(company))
+            print(company_to_string(company))
 
 
 if __name__ == '__main__':
